42d9fee19b124e19e0889485eea168d4a533b0ec
Adam Espinoza <adamespi@usc.edu>
2019-06-18 11:55:23 -0700

    Included event container and API call script

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import './App.css';
 
-import Header from './Header/Header'
+import Header from './Header/Header';
+import EventContainer from '../containers/EventContainer';
+
 function App() {
   return (
     <div>
     	<Header />
+    	
+    	<Route exact path="/" component={EventContainer} />
     </div>
   );
 }
---
+++ src/containers/EventContainer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class EventContainer extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			events: [],
+			loading: true,
+		}
+	}
+
+	componentWillMount() {
+		
+	}
+
+	render() {
+		return(
+			<div>
+			</div>
+		);
+	}
+}
+
+export default EventContainer
